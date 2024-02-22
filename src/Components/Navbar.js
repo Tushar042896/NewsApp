fb85e98a29008d25d53d3ed3270011b62b92e5d5
Tushar Jha <tushartk.jha@gmail.com>
2024-02-22 19:35:03 +0530

    changes in navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = ({setcategory}) => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary " data-bs-theme="dark">
       <div className="container-fluid">
         <a className="navbar-brand" href="#"><span className="badge text-bg-secondary"> Latest News</span>
         </a>
@@ -11,14 +11,14 @@ const Navbar = ({setcategory}) => {
         </button>
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
-            <li className="navbar-nav" >
-              <a className="nav-link" onClick={()=>setcategory("business")} >Business</a>
-              <a className="nav-link" onClick={()=>setcategory("entertainment")} >Entertainment</a>
-              <a className="nav-link" onClick={()=>setcategory("general")}>General</a>
-              <a className="nav-link" onClick={()=>setcategory("health")} >Health</a>
-              <a className="nav-link" onClick={()=>setcategory("science")}>Science</a>
-              <a className="nav-link " onClick={()=>setcategory("sports")}>Sports</a>
-              <a className="nav-link"  onClick={()=>setcategory("technology")}>Technology</a>
+            <li className="navbar-nav ">
+              <a className="nav-link" onClick={()=>setcategory("business") } style = {{cursor : "pointer"}}>Business</a>
+              <a className="nav-link" onClick={()=>setcategory("entertainment")} style = {{cursor : "pointer"}} >Entertainment</a>
+              <a className="nav-link" onClick={()=>setcategory("general")} style = {{cursor : "pointer"}}>General</a>
+              <a className="nav-link" onClick={()=>setcategory("health")} style = {{cursor : "pointer"}}>Health</a>
+              <a className="nav-link" onClick={()=>setcategory("science")}style = {{cursor : "pointer"}}>Science</a>
+              <a className="nav-link " onClick={()=>setcategory("sports")} style = {{cursor : "pointer"}}>Sports</a>
+              <a className="nav-link"  onClick={()=>setcategory("technology")} style = {{cursor : "pointer"}}>Technology</a>
 
             </li>
 
